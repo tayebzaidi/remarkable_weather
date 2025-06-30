ce23144c65eab455273732f7777d25e0932d2053
Tayeb Zaidi <tayebzaidi@gmail.com>
2025-06-30 00:53:58 -0500

    Fix directory structure

--- update_rm1_weather_image.sh
+++ update_rm1_weather_image.sh
@@ -7,7 +7,7 @@ RM1_CONFIG_NAME='remarkable'
 RM1_IP='192.168.4.171'   # Replace with the IP address of your RM1 device
 RM1_USER='root'        
 IMAGE_PATH='weather_forecast.png'
-REMOTE_IMAGE_PATH='~/Development/weather_items/poweroff.png'
+REMOTE_IMAGE_PATH='~/wi/poweroff.png'
 LOG="$HOME/Development/remarkable_weather/weather_update.log"
 
 timestamp() { date '+%F %T'; }
@@ -17,7 +17,7 @@ cd "$WORKDIR"
 "$PYTHON" gen_forecast_image.py
 
 # 2) Copy it to the tablet
-if scp -vv $IMAGE_PATH $RM1_USER@$RM1_CONFIG_NAME:$REMOTE_IMAGE_PATH; then
+if scp -q $IMAGE_PATH $RM1_USER@$RM1_CONFIG_NAME:$REMOTE_IMAGE_PATH; then
 	echo "$(timestamp) – power-off image UPDATED successfully" >> "$LOG"
 else
 	echo "$(timestamp) - ERROR: could not copy image (tablet unreachable)" >> "$LOG"
